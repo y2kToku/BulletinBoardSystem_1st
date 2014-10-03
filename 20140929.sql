60f2a36bd50212334fd4b85bbf4b60706577e1ee
Yusuke Tokuda <yu2ktoku@gmail.com>
2014-10-03 10:44:17 +0900

    スレッド管理テーブルにカラム「category_id」を追加しました。

--- 20140929.sql
+++ 20140929.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: 2014 年 9 月 29 日 17:18
+-- Generation Time: 2014 年 10 月 03 日 10:41
 -- サーバのバージョン： 5.5.38
 -- PHP Version: 5.3.28
 
@@ -20,16 +20,16 @@ SET time_zone = "+00:00";
 -- テーブルの構造 `categories`
 --
 
-CREATE TABLE IF NOT EXISTS `categories` (
+CREATE TABLE `categories` (
 `id` int(11) NOT NULL COMMENT 'カテゴリID',
   `title` varchar(255) NOT NULL COMMENT 'カテゴリ名称',
-  `cnt_comment` int(11) NOT NULL COMMENT 'コメント数',
+  `cnt_comment` int(11) NOT NULL DEFAULT '0' COMMENT 'コメント数',
   `creater` int(11) NOT NULL COMMENT '作成者',
   `created` datetime NOT NULL COMMENT '作成日時',
   `updater` int(11) NOT NULL COMMENT '更新者',
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新日時',
-  `del_flg` tinyint(1) NOT NULL COMMENT '削除フラグ'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='カテゴリ管理';
+  `del_flg` tinyint(1) NOT NULL DEFAULT '0' COMMENT '削除フラグ'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='カテゴリ管理' AUTO_INCREMENT=22 ;
 
 -- --------------------------------------------------------
 
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `categories` (
 -- テーブルの構造 `login_users`
 --
 
-CREATE TABLE IF NOT EXISTS `login_users` (
+CREATE TABLE `login_users` (
 `id` int(11) NOT NULL COMMENT 'ユーザID',
   `address` varchar(255) NOT NULL COMMENT 'メールアドレス',
   `name` varchar(32) NOT NULL COMMENT 'アカウント名',
@@ -48,13 +48,13 @@ CREATE TABLE IF NOT EXISTS `login_users` (
   `photo` varchar(255) DEFAULT NULL COMMENT '写真',
   `hint` varchar(32) DEFAULT NULL COMMENT 'ヒント',
   `admin_flg` tinyint(1) NOT NULL COMMENT '管理者フラグ',
-  `first_login_flg` tinyint(1) NOT NULL COMMENT '初回ログインフラグ',
+  `first_login_flg` tinyint(1) NOT NULL DEFAULT '0' COMMENT '初回ログインフラグ',
   `creater` int(11) NOT NULL COMMENT '作成者',
   `created` datetime NOT NULL COMMENT '作成日時',
   `updater` int(11) NOT NULL COMMENT '更新者',
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新日時',
-  `del_flg` tinyint(1) NOT NULL COMMENT '削除フラグ'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='ログインユーザ管理';
+  `del_flg` tinyint(1) NOT NULL DEFAULT '0' COMMENT '削除フラグ'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='ログインユーザ管理' AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -62,15 +62,16 @@ CREATE TABLE IF NOT EXISTS `login_users` (
 -- テーブルの構造 `threads`
 --
 
-CREATE TABLE IF NOT EXISTS `threads` (
+CREATE TABLE `threads` (
 `id` int(11) NOT NULL COMMENT 'スレッドID',
+  `category_id` int(11) NOT NULL COMMENT 'カテゴリID',
   `content` varchar(255) NOT NULL COMMENT 'スレッド内容',
   `creater` int(11) NOT NULL COMMENT '作成者',
   `created` datetime NOT NULL COMMENT '作成日時',
   `updater` int(11) NOT NULL COMMENT '更新者',
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
-  `del_flg` tinyint(1) NOT NULL COMMENT '削除フラグ'
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='スレッド管理';
+  `del_flg` tinyint(1) NOT NULL DEFAULT '0' COMMENT '削除フラグ'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='スレッド管理' AUTO_INCREMENT=29 ;
 
 --
 -- Indexes for dumped tables
@@ -86,7 +87,7 @@ ALTER TABLE `categories`
 -- Indexes for table `login_users`
 --
 ALTER TABLE `login_users`
- ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`), ADD UNIQUE KEY `address` (`address`), ADD UNIQUE KEY `address_2` (`address`), ADD UNIQUE KEY `address_3` (`address`);
 
 --
 -- Indexes for table `threads`
@@ -102,14 +103,14 @@ ALTER TABLE `threads`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'カテゴリID';
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'カテゴリID',AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT for table `login_users`
 --
 ALTER TABLE `login_users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ユーザID';
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ユーザID',AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `threads`
 --
 ALTER TABLE `threads`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'スレッドID',AUTO_INCREMENT=5;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'スレッドID',AUTO_INCREMENT=29;
